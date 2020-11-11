9540be3af0939d23d195cd00e440fd307ac2f536
miguelmc5 <mmartinezcosta@danielcastelao.org>
2020-11-11 09:42:54 +0100

    mi tema

--- html/wp-content/themes/MiPrimerTema/header.php
+++ html/wp-content/themes/MiPrimerTema/header.php
@@ -1,2 +1,16 @@
+<div id="header">
+    <div id="headerimg">
+        <h1>
+            <a href="<?php echo get_option('home'); ?>">
+				<?php bloginfo('name'); ?></a>
+        </h1>
+        <div class="description">
+			<?php bloginfo('description'); ?>
+            <br>
+            <img src="<?php bloginfo('stylesheet_directory'); ?>/images/fondo.jpg">
+        </div>
+    </div>
+</div>
 <?php
+
 echo "<h1>Headeeerrrrr</h1>";
